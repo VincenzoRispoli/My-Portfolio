1c49940e45510c80af17685779dcaa627fc74a2d
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-18 22:24:44 +0200

    new components

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -4,12 +4,13 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { IntroductionComponent } from './introduction/introduction.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, AboveTheFoldComponent, IntroductionComponent, SkillSetComponent,MyWorkComponent],
+  imports: [CommonModule, AboveTheFoldComponent, IntroductionComponent, SkillSetComponent,MyWorkComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
