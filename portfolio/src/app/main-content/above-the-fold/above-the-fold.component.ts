3a0b34308a9e84c9b68fbca508313e2be3886ccc
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-21 18:09:59 +0200

    new component

--- portfolio/src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ portfolio/src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
 
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, MenuMobileComponent],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
---
+++ portfolio/src/app/main-content/above-the-fold/menu-mobile/menu-mobile.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-mobile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-mobile.component.html',
+  styleUrl: './menu-mobile.component.scss'
+})
+export class MenuMobileComponent {
+  
+}
